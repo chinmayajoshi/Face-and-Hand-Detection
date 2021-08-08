def470eb8caef6f1ccde72de67b0343c1bfb44cc
Chinmaya Joshi <chinmayajoshi6@gmail.com>
2021-08-08 20:14:32 +0530

    updated handdetect.py

--- handdetect.py
+++ handdetect.py
@@ -1,21 +1,21 @@
 import cv2 as cv
 import mediapipe as mp
-
-
-# FaceMesh model object
-# performance decreases dramatically when creating an instance inside the function call
-mp_hands = mp.solutions.hands
-mh = mp_hands.Hands(static_image_mode=False, max_num_hands = 2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
  
 def detectHands(frame):
 
-    # for drawing detections over the origanl frame 
+    # for drawing detections over the original frame 
     mp_draw = mp.solutions.drawing_utils
 
-    # detecting face mesh
+    # because apparently the model likes BGR
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+        
+    # detecting hand coordinates
     frame_detections = mh.process(frame)
     
-    # modify the original frame object if detections available
+    # RGB ftw
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    
+    # modify the frame object if detections available
     if frame_detections.multi_hand_landmarks:
         for hand_landmarks in frame_detections.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
@@ -27,6 +27,11 @@ if __name__ == '__main__':
     # capture object 
     cap = cv.VideoCapture(0)
     
+    # FaceMesh model object
+    # performance decreases dramatically when creating an instance inside the function call
+    mp_hands = mp.solutions.hands
+    mh = mp_hands.Hands(static_image_mode=False, max_num_hands = 2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    
     # start capturing from cam
     while True:
         ret, frame = cap.read()
@@ -36,12 +41,10 @@ if __name__ == '__main__':
        
         # show original frame
         cv.imshow('frame', frame)
-
-        frame.flags.writeable = False
         
         # detect face via detectHands method
         hand_detect = detectHands(frame)
-
+        
         # show hands detected frame
         cv.imshow('hand_detection frame', hand_detect)
        
